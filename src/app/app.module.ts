d2e830cc5c2c4b1de58bb2b8ab9d9633686fa1da
Wojciech Surmacz <wojciech.surmacz@gmail.com>
2017-12-03 18:44:47 +0100

    Create project scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-
+import {ListComponent} from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ListComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ListComponent]
 })
 export class AppModule { }
